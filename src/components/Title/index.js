bfc63e4f9428e8026e429bca04074a5b3faa4e67
Artur Rodrigues <artur190503@outlook.com>
2023-11-23 15:49:31 -0300

    Estilizando o título do app

--- src/components/Title/index.js
+++ src/components/Title/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Text, View } from 'react-native';
+import styles from './style';
 
 export default function Title() {
     return (
-        <View>
-            <Text>HEALTH-HUB</Text>
+        <View style={styles.boxTittle}>
+            <Text style={styles.textTittle}>HEALTH-HUB</Text>
         </View>
     )
 }
---
+++ src/components/Title/style.js
@@ -0,0 +1,17 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+    boxTittle: {
+        alignItems: 'center',
+        justifyContent: 'center',
+        padding: 10,
+    },
+    textTittle: {
+        color: '#278C7A',
+        fontSize: 24,
+        fontWeight: 'bold',
+    }
+
+})
+
+export default styles
