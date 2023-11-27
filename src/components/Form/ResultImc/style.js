9012c3ff41222805147afeaad70fe2f219f2c866
Artur Rodrigues <artur190503@outlook.com>
2023-11-27 10:32:47 -0300

    Estlizando o output

---
+++ src/components/Form/ResultImc/style.js
@@ -0,0 +1,24 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+    resultImc: {
+        flex: 1,
+        marginTop: 15,
+        paddingTop: 60,
+        borderRadius: 50,
+        alignItems: 'center',
+        width: '100%',
+    },
+    numberImc:{
+        fontSize: 45,
+        color: '#278C7A',
+        fontWeight: 'bold',
+    },
+    information: {
+        fontSize: 15,
+        color: '#000000',
+        fontWeight: 'bold',
+    }
+})
+
+export default styles
